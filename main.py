65060c57cccd7cdae222be7ee7db2a045ffdc4a4
ChrisJMolloy <cmolloy22@gmail.com>
2018-02-03 16:55:38 -0500

    html complete
    finished start page functions

--- main.py
+++ main.py
@@ -1,1 +1,46 @@
-target = input('Target:')
+from flask import Flask,request, render_template
+import datetime
+import calendar
+app = Flask(__name__)
+@app.route("/")
+
+def main():
+    return render_template('qHack.html')
+
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+
+    start = request.form['start']
+    end = request.form['finish']
+    startTime = [int(a) for a in str(start).split('-')]
+    startFinal = datetime.datetime(startTime[0], startTime[1], startTime[2], 0, 0)
+    endTime = [int(a) for a in str(end).split('-')]
+    endFinal = datetime.datetime(endTime[0], endTime[1], endTime[2], 0, 0)
+
+
+
+    startForWatson = calendar.timegm(startFinal.timetuple())
+    endForWatson = calendar.timegm(endFinal.timetuple())
+
+    name = str(request.form['text'])
+    return start + name + end
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run()
+
+
+
+def check(start, end, name):
+    print(start + end + name)
